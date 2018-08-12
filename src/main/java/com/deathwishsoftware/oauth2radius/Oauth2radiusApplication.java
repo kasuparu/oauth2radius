7c4bb1d22b0fd5cd0aa50e0f4acce39027e4465d
kasuparu <kasuparu@about.me>
2018-08-12 21:31:13 +0300

    bare bones

---
+++ src/main/java/com/deathwishsoftware/oauth2radius/Oauth2radiusApplication.java
@@ -0,0 +1,12 @@
+package com.deathwishsoftware.oauth2radius;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Oauth2radiusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Oauth2radiusApplication.class, args);
+	}
+}
---
+++ src/test/java/com/deathwishsoftware/oauth2radius/Oauth2radiusApplicationTests.java
@@ -0,0 +1,16 @@
+package com.deathwishsoftware.oauth2radius;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Oauth2radiusApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
