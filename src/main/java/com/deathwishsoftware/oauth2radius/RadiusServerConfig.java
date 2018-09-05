6b1677d5695bf144835f9bdc348a43d2513677bd
kasuparu <kasuparu@about.me>
2018-09-05 22:59:50 +0300

    AuthPort as config variable

--- src/main/java/com/deathwishsoftware/oauth2radius/RadiusServerConfig.java
+++ src/main/java/com/deathwishsoftware/oauth2radius/RadiusServerConfig.java
@@ -1,16 +1,28 @@
 package com.deathwishsoftware.oauth2radius;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RadiusServerConfig {
 
+    @Value("${application.authPort:0}")
+    protected String authPortStr;
+
     @Bean
     public RadiusServerImpl getRadiusServer() {
         RadiusServerImpl server = new RadiusServerImpl();
-        // TODO Get server port from application config
-        server.setAuthPort(30000);
+        int authPort = 0;
+        try {
+            authPort = Integer.parseInt(authPortStr);
+        } catch (Exception e) {
+            // TODO Log an error
+        } finally {
+            if (authPort != 0) {
+                server.setAuthPort(authPort);
+            }
+        }
         return server;
     }
 
--- src/test/java/com/deathwishsoftware/oauth2radius/RadiusIntegrationTestBase.java
+++ src/test/java/com/deathwishsoftware/oauth2radius/RadiusIntegrationTestBase.java
@@ -1,18 +1,33 @@
 package com.deathwishsoftware.oauth2radius;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.test.context.ActiveProfiles;
 import org.tinyradius.packet.AccessRequest;
 import org.tinyradius.packet.RadiusPacket;
 import org.tinyradius.util.RadiusClient;
 
-public class RadiusIntegrationTestBase {
+@ActiveProfiles("test")
+public abstract class RadiusIntegrationTestBase {
 
     protected RadiusClient client;
+
+    @Value("${application.authPort:0}")
+    protected String authPortStr;
 
     RadiusIntegrationTestBase() {
         // TODO Get params from test profile config
         this.client = new RadiusClient("localhost", "shared-secret");
-        // TODO Get server port from application config
-        this.client.setAuthPort(30000);
+
+        int authPort = 0;
+        try {
+            authPort = Integer.parseInt(authPortStr);
+        } catch (Exception e) {
+            // TODO Log an error
+        } finally {
+            if (authPort != 0) {
+                this.client.setAuthPort(authPort);
+            }
+        }
     }
 
     protected RadiusPacket makeRequest(String userName, String userPassword) throws Exception {
