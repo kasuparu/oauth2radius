fc4f798c49e7a21d20cfb3ce077b6893e4eedcd0
kasuparu <kasuparu@about.me>
2018-09-17 23:10:34 +0300

    OAuth2

--- src/main/java/com/deathwishsoftware/oauth2radius/controller/IndexController.java
+++ src/main/java/com/deathwishsoftware/oauth2radius/controller/IndexController.java
@@ -4,13 +4,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.security.Principal;
+
 @RestController
-@RequestMapping("/")
 public class IndexController {
 
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @RequestMapping("/user")
+    public Principal user(Principal principal) {
+        return principal;
     }
 
 }
