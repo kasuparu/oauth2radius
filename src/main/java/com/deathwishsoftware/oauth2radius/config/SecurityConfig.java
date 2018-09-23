1e981dfe5679f60500bbc10a1a9f427bb3f98334
kasuparu <kasuparu@about.me>
2018-09-23 19:24:42 +0300

    Google verification file

--- src/main/java/com/deathwishsoftware/oauth2radius/config/SecurityConfig.java
+++ src/main/java/com/deathwishsoftware/oauth2radius/config/SecurityConfig.java
@@ -52,7 +52,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         return registration;
     }
 
-    private static final String[] WHITE_LIST = {"/", "/login**", "/error**", "/favicon.ico", "/google**"};
+    private static final String[] WHITE_LIST = {"/", "/login**", "/error**", "/favicon.ico", "/google9a147766ec793ea5.html"};
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
