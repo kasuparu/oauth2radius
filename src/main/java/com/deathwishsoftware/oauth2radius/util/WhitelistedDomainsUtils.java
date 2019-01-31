3568dbe02596ee4160b483fafbf4799d5f5b0900
kasuparu <kasuparu@about.me>
2019-01-31 16:29:25 +0200

    Print whitelisted domains on startup

--- src/main/java/com/deathwishsoftware/oauth2radius/util/WhitelistedDomainsUtils.java
+++ src/main/java/com/deathwishsoftware/oauth2radius/util/WhitelistedDomainsUtils.java
@@ -1,15 +1,30 @@
 package com.deathwishsoftware.oauth2radius.util;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
+
 @Component
 public class WhitelistedDomainsUtils {
+
+    private static Logger logger = Logger.getLogger(WhitelistedDomainsUtils.class);
 
     private String[] urls;
 
     public WhitelistedDomainsUtils(@Value("${oauth2radius.whitelisted-domains}") String[] urls) {
         this.urls = urls;
+    }
+
+    @PostConstruct
+    public void printDomains() {
+        if (urls.length > 0) {
+            logger.info(String.format("%s", Arrays.toString(urls)));
+        } else {
+            logger.error("No whitelisted domains specified, credential creation is impossible");
+        }
     }
 
     public boolean isAllowed(String email) {
