8d893278535a26019b0819d9fd0170e06880976f
kasuparu <kasuparu@about.me>
2019-03-21 18:49:14 +0200

    Bugfix: do not create a password for non-whitelisted email

--- src/main/java/com/deathwishsoftware/oauth2radius/config/AuthenticationListener.java
+++ src/main/java/com/deathwishsoftware/oauth2radius/config/AuthenticationListener.java
@@ -3,6 +3,7 @@ package com.deathwishsoftware.oauth2radius.config;
 import com.deathwishsoftware.oauth2radius.persistence.RadCheckService;
 import com.deathwishsoftware.oauth2radius.util.AuthenticationProperties;
 import com.deathwishsoftware.oauth2radius.util.OAuth2AuthenticationUtils;
+import com.deathwishsoftware.oauth2radius.util.WhitelistedDomainsUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.EventListener;
@@ -14,14 +15,22 @@ import org.springframework.security.oauth2.provider.OAuth2Authentication;
 public class AuthenticationListener {
 
     @Autowired
-    protected RadCheckService radCheckService;
+    private RadCheckService radCheckService;
+
+    @Autowired
+    private WhitelistedDomainsUtils whitelistedDomainsUtils;
 
     @EventListener
     public void authSuccessEventListener(AuthenticationSuccessEvent authorizedEvent) {
         Authentication auth = authorizedEvent.getAuthentication();
-        if (auth instanceof OAuth2Authentication) {
-            AuthenticationProperties properties = OAuth2AuthenticationUtils.extractAuthenticationProperties((OAuth2Authentication)auth);
-            this.radCheckService.generateUserPasswordIfNeeded(properties.getEmail());
+        if (!(auth instanceof OAuth2Authentication)) {
+            return;
+        }
+
+        AuthenticationProperties properties = OAuth2AuthenticationUtils.extractAuthenticationProperties((OAuth2Authentication)auth);
+        String email = properties.getEmail();
+        if (whitelistedDomainsUtils.isAllowed(email)) {
+            this.radCheckService.generateUserPasswordIfNeeded(email);
         }
     }
 
